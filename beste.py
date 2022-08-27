83617aeb1d7384d4535937ca1e496f9c9a7f5a87
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-08-27 23:21:30 +0200

    Update beste.py

--- beste.py
+++ beste.py
@@ -5,7 +5,7 @@ uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "(/?:_-*+,.ˇ%"
 string = lowercase + uppercase + numbers + symbols
-length = 32
+length = 50
 password = "".join(random.sample(string,length))
 
-print("your new password master owo" + password)
+print("your new password master owo  " + password)
